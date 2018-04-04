abb2f9bee9334e0a47eb66d67f66aa5774be7bd9
zhushunsheng <503305196@qq.com>
2018-04-04 15:29:30 +0800

    +结构调整

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="page-login">
-      <div class="top"></div>
+      <div class="top">
+        <h2>博文管理后台</h2>
+      </div>
       <el-form>
           <el-form-item>
               <el-input v-model="user.account" placeholder="请输入账号"></el-input>
@@ -44,6 +46,14 @@ export default {
         width: 100%;
         height: 80px;
         background: #333;
+        box-shadow: 0 2px 2px rgba(0, 0, 0, .2);
+        h2{
+            margin: 0;
+            padding: 0 30px;
+            color: #FFF;
+            line-height: 80px;
+            font-weight: normal;
+        }
     }
     .el-form{
         width: 300px;
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -4,6 +4,11 @@
         <h2>博文管理后台</h2>
       </div>
       <div class="left">
+        <ul class="nav">
+          <li>
+            <router-link :to="{name:'blogList'}">博客管理</router-link>
+          </li>
+        </ul>
       </div>
       <div class="contnet">
          <router-view/>
@@ -34,6 +39,20 @@
      background: #FFF;
      border-right:1px solid #E6E6E6;
      box-shadow: 2px 0  2px rgba(0,0,0,0.1);
+     .nav{
+       padding-top:80px;
+       > li a{
+         display: block;
+         padding: 0 30px;
+         height: 50px;
+         line-height: 50px;
+         text-decoration: none;
+         color: #333;
+         &:hover{
+           background: #EEE;
+         }
+       }
+     }
    }
    .contnet{
      padding: 80px 0 0 200px;
